52bdcd96f556e0f130269b58a472cdaaf98d8fcb
Jeff Gonzalez <jeff@hypertheory.com>
2022-09-07 16:19:28 -0400

    creating users

---
+++ UsersBoundedContext/UsersSolution/UsersApi/Controllers/WebPresenceProcessorController.cs
@@ -0,0 +1,11 @@
+﻿using HypertheoryMessages.WebPresence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersApi.Controllers;
+
+public class WebPresenceProcessorController : ControllerBase
+{
+
+    [HttpPost("/web-presence-processor/user-onboarded")]
+    public async Task<ActionResult> MakeAUserOutOfThisWebPerson([FromBody] WebUser)
+}
